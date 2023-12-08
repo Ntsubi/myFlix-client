a79c89083bae92246717a2d7496f387d2a20c668
Ntsubi <145991816+Ntsubi@users.noreply.github.com>
2023-12-08 11:06:19 +0100

    Modified the MainView component

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,4 +1,6 @@
 import { useState } from "react";
+import { MovieCard } from "../movie-card/movie-card";
+import { MovieView } from "../movie-view/movie-view";
 
 export const MainView = () => {
   const [movies, setMovies] = useState([
@@ -58,7 +60,34 @@ export const MainView = () => {
     }
   ]);
 
+  const [selectedMovie, setSelectedMovie] = useState(null);
 
+  if (selectedMovie) {
+    return (
+      <MovieView
+        movie={selectedMovie}
+        onBackClick={() => setSelectedMovie(null)}
+      />
+    );
+  }
 
-
-}
+  if (movies.length === 0) {
+    return <div>This list is empty</div>;
+  } else {
+    return (
+      <div>
+        {movies.map((movie) => {
+          return (
+            <MovieCard
+              key={movie.id}
+              movie={movie}
+              onMovieClick={(newSelectedMovie) => {
+                setSelectedMovie(newSelectedMovie);
+              }}
+            />
+          );
+        })}
+      </div>
+    );
+  }
+};
