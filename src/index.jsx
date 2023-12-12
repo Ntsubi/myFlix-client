b39eb0afd46189146f6ad3b8e9250ac521c63802
Ntsubi <145991816+Ntsubi@users.noreply.github.com>
2023-12-12 15:24:45 +0100

    Edited import statement

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
 import { createRoot } from 'react-dom/client';
 import MainView from "./components/main-view/main-view";
-import { MovieView } from "./components/movie-view/movie-view";
+import MovieView from "./components/movie-view/movie-view";
 import './index.scss'; //import statement that you need to bundle './index.scss'
 
 //main component (will eventually use all the others)
