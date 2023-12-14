a68c922582460e4136725c1efa450c0a62357788
Ntsubi <145991816+Ntsubi@users.noreply.github.com>
2023-12-14 13:38:06 +0100

    Imported login & signup view components

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,8 @@
 import { createRoot } from 'react-dom/client';
 import MainView from "./components/main-view/main-view";
 import MovieView from "./components/movie-view/movie-view";
+import LoginView from "./components/loginview/loginview";
+import SignupView from "./components/signup-view/signup-view";
 import './index.scss'; //import statement that you need to bundle './index.scss'
 
 //main component (will eventually use all the others)
