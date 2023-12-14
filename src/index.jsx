b06d688626710faed15204469bf52dbdbc1a7e9f
Ntsubi <145991816+Ntsubi@users.noreply.github.com>
2023-12-14 13:48:47 +0100

    Removed redundant code

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,6 @@
 import { createRoot } from 'react-dom/client';
 import MainView from "./components/main-view/main-view";
 import MovieView from "./components/movie-view/movie-view";
-import LoginView from "./components/login-view/login-view";
-import SignupView from "./components/signup-view/signup-view";
 import './index.scss'; //import statement that you need to bundle './index.scss'
 
 //main component (will eventually use all the others)
