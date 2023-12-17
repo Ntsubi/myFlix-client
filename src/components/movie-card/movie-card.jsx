0777d31635cd2f3ef17116e6db08a44647fae19c
Ntsubi <145991816+Ntsubi@users.noreply.github.com>
2023-12-17 12:52:44 +0100

    Integrated Bootstrap styling

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,14 +1,18 @@
 import PropTypes from 'prop-types';
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
 
 const MovieCard = ({ movie, onMovieClick }) => {
+
   return (
-    <div
-      onClick={() => {
-        onMovieClick(movie);
-      }}
-    >
-      {movie.Title}
-    </div>
+    <Card>
+      <Card.Img variant="top" src={movie.ImageURL} />
+      <Card.Body>
+        <Card.Title>{movie.Title}</Card.Title>
+        <Card.Title>{movie.Genre.Name}</Card.Title>
+        <Button onClick={() => onMovieClick(movie)} variant="primary" size="sm">Open</Button>
+      </Card.Body>
+    </Card>
   );
 };
 
