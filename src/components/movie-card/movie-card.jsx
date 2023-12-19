2991ec3837f037a6489056de5c28068def6b6665
Ntsubi <145991816+Ntsubi@users.noreply.github.com>
2023-12-19 14:55:20 +0100

    Removed onMovieClick function

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -2,7 +2,7 @@ import PropTypes from 'prop-types';
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
 
-const MovieCard = ({ movie, onMovieClick }) => {
+const MovieCard = ({ movie }) => {
 
   return (
     <Card>
@@ -10,7 +10,7 @@ const MovieCard = ({ movie, onMovieClick }) => {
       <Card.Body>
         <Card.Title>{movie.Title}</Card.Title>
         <Card.Title>{movie.Genre.Name}</Card.Title>
-        <Button onClick={() => onMovieClick(movie)} variant="primary" size="sm">Open</Button>
+        <Button variant="primary" size="sm">Open</Button>
       </Card.Body>
     </Card>
   );
@@ -20,7 +20,6 @@ MovieCard.propTypes = {
   movie: PropTypes.shape({
     Title: PropTypes.string.isRequired
   }).isRequired,
-  onMovieClick: PropTypes.func.isRequired
 };
 
 export default MovieCard;
