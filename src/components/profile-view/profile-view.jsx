bb1e717120b763ea3565beef1efc3018d223b9c7
Ntsubi <145991816+Ntsubi@users.noreply.github.com>
2023-12-20 15:42:29 +0100

    Defined function for updating user info

--- src/components/profile-view/profile-view.jsx
+++ src/components/profile-view/profile-view.jsx
@@ -13,4 +13,41 @@ const ProfileView = () => {
   const [email, setEmail] = useState("");
   const [birthday, setBirthday] = useState("");
 
+  const handleUpdate = (event) => {
+    event.preventDefault();
+
+
+    const data = {
+      Username: username,
+      Password: password,
+      Email: email,
+      Birthday: birthday
+    }
+    //Update user info
+    fetch(`https://render-movie-api.onrender.com/users/${user.username}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+        headers: { Authorization: `Bearer ${token}` }
+      },
+      body: JSON.stringify(data)
+    })
+      .then((response) => {
+        if (response.ok) {
+          alert('Profile successfully updated');
+        } else {
+          alert('Update failed')
+        }
+      })
+      .catch((err) => {
+        console.error(err);
+      })
+  };
+
+
+
 };
+
+
+
+
