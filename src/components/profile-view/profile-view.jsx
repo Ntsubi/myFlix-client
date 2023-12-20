68722a70131dcd7f0c29d4916e9a81fd39512199
Ntsubi <145991816+Ntsubi@users.noreply.github.com>
2023-12-20 09:10:19 +0100

    Created profile view & imported React components

---
+++ src/components/profile-view/profile-view.jsx
@@ -0,0 +1,7 @@
+import { React, useState } from "react";
+import MovieCard from "../movie-card/movie-card";
+import { Link } from "react-router-dom";
+import Col from "react-bootstrap/Col";
+import Form from "react-bootstrap/Form";
+import Card from "react-bootstrap/Button";
+import Button from "react-bootstrap/Button";
