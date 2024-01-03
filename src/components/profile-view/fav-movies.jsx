5b8e7ce88117ae01c2e4f47b0611bf45913b97ab
Ntsubi <145991816+Ntsubi@users.noreply.github.com>
2024-01-03 11:10:21 +0100

    Passed setUser prop

--- src/components/profile-view/fav-movies.jsx
+++ src/components/profile-view/fav-movies.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import MovieCard from "../movie-card/movie-card";
 
-const FavoriteMovies = ({ favoriteMovies }) => {
+const FavoriteMovies = ({ favoriteMovies, setUser }) => {
 
   return (
     <>
       <h5>Favorites</h5>
       {
         favoriteMovies.map(movie => {
-          return <MovieCard movie={movie} key={movie._id} />
+          return <MovieCard movie={movie} key={movie._id} setUser={setUser} />
 
         })
       }
