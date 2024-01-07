29b588f94cd4fe48855e0dee39941dda161c090c
Ntsubi <145991816+Ntsubi@users.noreply.github.com>
2024-01-07 11:27:12 +0100

    Added search bar

--- src/components/nav-bar/nav-bar.jsx
+++ src/components/nav-bar/nav-bar.jsx
@@ -2,6 +2,10 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import { Link } from "react-router-dom";
+import Form from "react-bootstrap/Form";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Button from "react-bootstrap/Button";
 
 const NavBar = ({ user, onLoggedOut }) => {
 
@@ -23,8 +27,25 @@ const NavBar = ({ user, onLoggedOut }) => {
                 <Nav.Link as={Link} to="/">Home</Nav.Link>
                 <Nav.Link as={Link} to="/profile">Profile</Nav.Link>
                 <Nav.Link onClick={onLoggedOut}>Logout</Nav.Link>
+
               </>
             )}
+            <>
+              <Form className="form-inline" >
+                <Row>
+                  <Col xs="auto">
+                    <Form.Control
+                      type="text"
+                      placeholder="Search"
+                      className=" mr-sm-2"
+                    />
+                  </Col>
+                  <Col xs="auto">
+                    <Button type="submit">Submit</Button>
+                  </Col>
+                </Row>
+              </Form>
+            </>
           </Nav>
         </Navbar.Collapse>
       </Container>
