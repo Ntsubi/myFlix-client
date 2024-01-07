b2242b4c83dfadf3f245d98824a7a74fc0fac715
Ntsubi <145991816+Ntsubi@users.noreply.github.com>
2024-01-07 15:21:57 +0100

    Defined search bar event listener

--- src/components/nav-bar/nav-bar.jsx
+++ src/components/nav-bar/nav-bar.jsx
@@ -7,7 +7,11 @@ import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Button from "react-bootstrap/Button";
 
-const NavBar = ({ user, onLoggedOut }) => {
+const NavBar = ({ user, onLoggedOut, searchQuery, setSearchQuery }) => {
+
+  const handleSearch = (e) => {
+    setSearchQuery(e.target.value);
+  }
 
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
@@ -24,28 +28,27 @@ const NavBar = ({ user, onLoggedOut }) => {
             )}
             {user && (
               <>
+                <Form className="form-inline">
+                  <Row>
+                    <Col xs="auto">
+                      <Form.Control
+                        type="text"
+                        placeholder="Search"
+                        className=" mr-sm-2"
+                        value={searchQuery}
+                        onChange={handleSearch}
+                      />
+                    </Col>
+                    <Col xs="auto">
+                      <Button type="submit">Submit</Button>
+                    </Col>
+                  </Row>
+                </Form>
                 <Nav.Link as={Link} to="/">Home</Nav.Link>
                 <Nav.Link as={Link} to="/profile">Profile</Nav.Link>
                 <Nav.Link onClick={onLoggedOut}>Logout</Nav.Link>
-
               </>
             )}
-            <>
-              <Form className="form-inline" >
-                <Row>
-                  <Col xs="auto">
-                    <Form.Control
-                      type="text"
-                      placeholder="Search"
-                      className=" mr-sm-2"
-                    />
-                  </Col>
-                  <Col xs="auto">
-                    <Button type="submit">Submit</Button>
-                  </Col>
-                </Row>
-              </Form>
-            </>
           </Nav>
         </Navbar.Collapse>
       </Container>
